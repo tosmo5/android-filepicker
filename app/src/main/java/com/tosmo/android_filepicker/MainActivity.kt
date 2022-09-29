a8b0a06ba1db30d1632432a3424658b8c05eab06
tosmo5 <tosmo5@163.com>
2022-09-29 16:23:51 +0800

    first

--- app/src/main/java/com/tosmo/android_filepicker/MainActivity.kt
+++ app/src/main/java/com/tosmo/android_filepicker/MainActivity.kt
@@ -2,10 +2,12 @@ package com.tosmo.android_filepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tosmo.afilepicker.FilePicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FilePicker.STORAGE_PATH
     }
 }
